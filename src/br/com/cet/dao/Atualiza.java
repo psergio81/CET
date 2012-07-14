b1ec547eca62570e8fb7ea91ddf22ef87eb1ce13
Paulo Santos <psergio81@hotmail.com>
2012-07-14 13:09:40 -0300

    melhorias na classe Atualiza.java

--- src/br/com/cet/dao/Atualiza.java
+++ src/br/com/cet/dao/Atualiza.java
@@ -8,6 +8,16 @@ import java.sql.Statement;
 
 public class Atualiza extends BaseDao {
 
+	private static final String LINHA = "--------------------------------------------------------------------------------------";
+	private static final String LINHA_DUPLA = "======================================================================================";
+	private static final String TAMANHO_DO_CAMPO = "Tamanho do Campo";
+	private static final String TIPO_DO_CAMPO = "Tipo do Campo";
+	private static final String NOME_DO_CAMPO = "Nome do Campo";
+	private static final String NOME_DA_TABELA = "Nome da Tabela";
+	private static final String NOME_DO_BANCO = "Nome do Banco";
+	private static final String CRIANDO_SCHEMA = "Criando Schema";
+	private static final String CRIANDO_TABELA = "Criando Tabela";
+	private static final String CRIANDO_CAMPO = "Criando Campo";
 	private static final String NOME_BANCO = "ensaio";
 	private Connection connection;
 	private ResultSet rs;
@@ -19,14 +29,16 @@ public class Atualiza extends BaseDao {
 		
 		Atualiza atualiza = new Atualiza();
 		
-		atualiza.criarCampo(TipoCampo.VARCHAR, NOME_BANCO, "teste","campoVARCHAR",300, true);
-		atualiza.criarCampo(TipoCampo.INTEIRO, NOME_BANCO, "teste","campoINTEIRO",300, true);
-		atualiza.criarCampo(TipoCampo.INTEIRO, NOME_BANCO, "teste","campoINTEIRO2",300, true);
+		atualiza.criarSchema("novoBanco");
+		atualiza.criarTabela("novoBanco", "tabelaTeste");
+		atualiza.criarCampo(TipoCampo.VARCHAR, "novoBanco", "tabelaTeste","campoVARCHAR",300, false);
+		atualiza.criarCampo(TipoCampo.INTEIRO, "novoBanco", "tabelaTeste","campoINTEIRO",300, false);
+		atualiza.criarCampo(TipoCampo.INTEIRO, "novoBanco", "tabelaTeste","campoINTEIRO2",300, false);
 		
 	}
 	
-	@SuppressWarnings("unused")
 	private void criarTabela(String nomeBanco, String nomeTabela) {
+		printLog(CRIANDO_TABELA, nomeBanco, nomeTabela);
 	    try {  
 	    	connection = getConnection();
 
@@ -42,15 +54,30 @@ public class Atualiza extends BaseDao {
 	    } 
 	}
 
-	
+	private void criarSchema(String nomeBanco) {
+		printLog(CRIANDO_SCHEMA,nomeBanco);
+		try {  
+			connection = getConnection();
+			
+			qry = String.format("CREATE SCHEMA %s ;", nomeBanco);
+			
+			Statement statement = connection.createStatement();
+			statement.execute(qry);
+			
+		}catch (Exception e) {  
+			e.printStackTrace();
+		}finally {
+			releaseResouces(connection, ps, rs);
+		} 
+	}
+
 	private void criarCampo(TipoCampo tipoCampo, String nomeBanco, String nomeTabela, String nomeCampo, int tamanho, boolean campoCriado) {
 		
 		if(campoCriado){
 			return;
 		}
 		
-		
-		printLog(tipoCampo, nomeBanco, nomeTabela, nomeCampo, tamanho);
+		printLog(CRIANDO_CAMPO,tipoCampo, nomeBanco, nomeTabela, nomeCampo, tamanho);
 		
 		switch (tipoCampo) {
 		case INTEIRO:
@@ -99,19 +126,45 @@ public class Atualiza extends BaseDao {
 			releaseResouces(connection, ps, rs);
 		} 
 	}
+
+	private void printLog(String mensagem, String nomeBanco, String nomeTabela) {
+		this.printLog(mensagem, null, nomeBanco, nomeTabela, null, 0);
+	}
 	
-	private void printLog(TipoCampo tipoCampo, String nomeBanco, String nomeTabela, String nomeCampo, int tamanho) {
+	private void printLog(String mensagem, String nomeBanco) {
+		this.printLog(mensagem, null, nomeBanco, null, null, 0);
+	}
+	
+	private void printLog(String mensagem, TipoCampo tipoCampo, String nomeBanco, String nomeTabela, String nomeCampo, int tamanho) {
 		
-		System.out.println("======================================================================================");
-		System.out.println("                             CRIANDO CAMPO                                            ");
-		System.out.println("======================================================================================");
-		System.out.println("NOME BANCO	:		"+nomeBanco);
-		System.out.println("NOME TABELA	:		"+nomeTabela);
-		System.out.println("NOME CAMPO	:		"+nomeCampo);
-		System.out.println("TAMANHO		:		"+tamanho);
-		System.out.println("TIPO		:		"+tipoCampo);
-		System.out.println("--------------------------------------------------------------------------------------");
+		System.out.println(LINHA_DUPLA);
+		System.out.println("                             "+mensagem+"                                             ");
+		System.out.println(LINHA_DUPLA);
+		imprimeCampo(NOME_DO_BANCO, nomeBanco);
+		imprimeCampo(NOME_DA_TABELA, nomeTabela);	
+		imprimeCampo(NOME_DO_CAMPO, nomeCampo);
+		imprimeCampo(TIPO_DO_CAMPO, tipoCampo);
+		imprimeCampo(TAMANHO_DO_CAMPO, tamanho);
+		System.out.println(LINHA);
 		
+	}
+
+	private void imprimeCampo(String mensagem, TipoCampo parametro) {
+		if(parametro != null){
+			System.out.println(mensagem + "	:		"+parametro);
+		}
+	}
+
+	private void imprimeCampo(String mensagem, String parametro) {
+		if(parametro != null && parametro != ""){
+			System.out.println(mensagem + "	:		"+parametro);
+		}
+	}
+
+	private void imprimeCampo(String mensagem, int parametro) {
+		if(parametro != 0){
+			System.out.println(mensagem + "	:		"+parametro);
+		}
 	}
 	
 	
