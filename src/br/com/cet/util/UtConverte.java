4aea0b92ece35f5dc70ea44788279f1755648fc4
Paulo Santos <psergio81@hotmail.com>
2012-07-19 00:05:18 -0300

    criação das classes utilitarias para conversão de tipos e operações com String

---
+++ src/br/com/cet/util/UtConverte.java
@@ -0,0 +1,26 @@
+package br.com.cet.util;
+
+public class UtConverte extends UtString{
+
+	
+	/**
+	 * Metodo para converter uma string numerica em um numero inteiro
+	 * @param stringNumero
+	 * @return
+	 */
+	public static int stringToInteiro(String stringNumero){
+		
+		try {
+
+			if(isNullOrEmpty(stringNumero)){
+				return 0;
+			}
+			
+			return Integer.parseInt(stringNumero);
+			
+		} catch (Exception e) {
+			return 0;
+		}
+	}
+
+}
---
+++ src/br/com/cet/util/UtString.java
@@ -0,0 +1,98 @@
+package br.com.cet.util;
+
+public class UtString {
+
+	/**
+	 * Metodo que verifica se uma string é nula ou vazia
+	 * @param string
+	 * @return
+	 */
+	public static boolean isNullOrEmpty(String string){
+		
+		if(string == null || string.equals("")){
+			return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * 
+	 * @param stringA
+	 * @param stringB
+	 * @return
+	 */
+	public static boolean equals(String stringA, String ... stringB){
+		
+		if(isNullOrEmpty(stringA)){
+			return false;
+		}
+		
+		for (String string : stringB) {
+			if(!stringA.equals(string)){
+				return false;
+			}
+		}
+		return true;
+	}
+
+	/**
+	 * 
+	 * @param stringA
+	 * @param stringB
+	 * @return
+	 */
+	public static boolean equalsIgnoreCase(String stringA, String ... stringB){
+		
+		if(isNullOrEmpty(stringA)){
+			return false;
+		}
+		
+		for (String string : stringB) {
+			if(!stringA.equalsIgnoreCase(string)){
+				return false;
+			}
+		}
+		return true;
+	}
+
+	/**
+	 * 
+	 * @param stringA
+	 * @param stringB
+	 * @return
+	 */
+	public static boolean equalsOR(String stringA, String ... stringB){
+		
+		if(isNullOrEmpty(stringA)){
+			return false;
+		}
+		
+		for (String string : stringB) {
+			if(stringA.equals(string)){
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * 
+	 * @param stringA
+	 * @param stringB
+	 * @return
+	 */
+	public static boolean equalsORIgnoreCase(String stringA, String ... stringB){
+		
+		if(isNullOrEmpty(stringA)){
+			return false;
+		}
+		
+		for (String string : stringB) {
+			if(stringA.equalsIgnoreCase(string)){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+}
